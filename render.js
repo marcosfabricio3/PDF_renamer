3e688c5d60d9fbb0f74dfd26c6f4fbe5bc9c03d4
marcosfabricio3 <marcosfabricio633@gmail.com>
2025-12-08 10:11:47 -0300

    feat: añadir selector de PDF con click y drag&drop

--- render.js
+++ render.js
@@ -1,61 +1,63 @@
 // header
-const lenguageChangerBtn = document.getElementById("lenguageChangeBtn");
-const themeToggle = document.getElementById("themeToggle");
+const lenguageChangerBtn = document.getElementById("lenguageChangeBtn")
+const themeToggle = document.getElementById("themeToggle")
 
 // main
-const selectorPdf = document.getElementById("selectorPdf");
-const selectedFile = document.getElementById("selectedFile");
-const checkboxContainer = document.getElementById("checkboxContainer");
-const includeDateCheckbox = document.getElementById("includeDateCheckbox");
-const checkMalwareCheckbox = document.getElementById("checkMalwareCheckbox");
-const renameBtn = document.getElementById("renameBtn");
-const statusMessage = document.getElementById("statusMessage");
+const selectorPdf = document.getElementById("selectorPdf")
+const pdfFileInput = document.getElementById("pdfFileInput")
+const selectedFile = document.getElementById("selectedFile")
+const checkboxContainer = document.getElementById("checkboxContainer")
+const includeDateCheckbox = document.getElementById("includeDateCheckbox")
+const checkMalwareCheckbox = document.getElementById("checkMalwareCheckbox")
+const renameBtn = document.getElementById("renameBtn")
+const statusMessage = document.getElementById("statusMessage")
 
 // aside
-const profilesAndListContainer = document.getElementById("profilesAndListContainer");
-const newProfileBtn = document.getElementById("newProfileBtn");
+const profilesAndListContainer = document.getElementById("profilesAndListContainer")
+const newProfileBtn = document.getElementById("newProfileBtn")
 
 // input para agregar nombre
 const containerInputNewProfile = document.getElementById('containerInputNewProfile')
 const newProfileInput = document.getElementById('newProfileInput')
 const addProfileBtn = document.getElementById('addProfileBtn')
 
-let profiles = [];
-let selectedProfile = null;
+let profiles = []
+let selectedProfile = null
+let selectedFilePath = null
 
 function renderProfiles() {
-  profilesAndListContainer.innerHTML = "";
+  profilesAndListContainer.innerHTML = ""
 
   profiles.forEach((profileName) => {
     const button = document.createElement("button");
     button.textContent = profileName;
 
     button.addEventListener("click", () => {
-      selectedProfile = profileName;
+      selectedProfile = profileName
 
-      const allButtons = profilesAndListContainer.querySelectorAll("button");
-      allButtons.forEach((btn) => btn.classList.remove("profileActive"));
+      const allButtons = profilesAndListContainer.querySelectorAll("button")
+      allButtons.forEach((btn) => btn.classList.remove("profileActive"))
 
-      button.classList.add("profileActive");
+      button.classList.add("profileActive")
     });
 
-    profilesAndListContainer.appendChild(button);
+    profilesAndListContainer.appendChild(button)
   });
 }
 
 function addNameToList(name) {
   if (typeof name !== "string" || !name.trim()) {
-    return console.log("Nombre Invalido");
+    return console.log("Nombre Invalido")
   }
 
-  const nameClean = name.trim();
-  const nameToComparate = nameClean.toLocaleLowerCase()
+  const nameClean = name.trim()
+  const nameToComparate = nameClean.toLowerCase()
 
-  if(profiles.some(p => p.toLocaleLowerCase() === nameToComparate)) {
+  if(profiles.some(p => p.toLowerCase() === nameToComparate)) {
     return console.log('nombre repetido ', nameToComparate)
   }
 
-  profiles.push(nameClean);
+  profiles.push(nameClean)
   console.log("nombre almasenado ", nameClean);
   containerInputNewProfile.classList.add('hidden')
   renderProfiles();
@@ -87,3 +89,57 @@ addProfileBtn.addEventListener('click', () => {
   addNameToList(newProfile)
   newProfileInput.value = ""
 })
+
+// ------------------------------------------------------------------------------------------------
+
+selectorPdf.addEventListener("click", (e) => {
+  pdfFileInput.click()
+})
+
+pdfFileInput.addEventListener("change", (e) => {
+  const file = pdfFileInput.files[0]
+
+  if (!file){return console.log('no se selecciono file')}
+
+  handleFile(file)
+})
+
+// -------------------------
+
+selectorPdf.addEventListener('dragover', (e) => {
+  e.preventDefault()
+
+  e.dataTransfer.dropEffect = 'copy'
+  selectorPdf.classList.add('dragover')
+})
+
+selectorPdf.addEventListener('dragleave', (e) => {
+  selectorPdf.classList.remove('dragover')
+})
+
+selectorPdf.addEventListener('drop', (e) => {
+  e.preventDefault()
+  selectorPdf.classList.remove('dragover')
+
+  const file = e.dataTransfer.files[0]
+
+  if (!file) {
+    return console.log('no se solto ningun archivo')
+  }
+
+  handleFile(file)
+})
+
+// -------------------------
+
+function handleFile (file) {
+
+  const isPdf = file.type === 'application/pdf'
+  if (!isPdf) {return console.log('el archivo no es PDF')}
+
+  selectedFilePath = file.name
+  console.log('el path es ', selectedFilePath)
+  
+  selectedFile.textContent = `El nombre del archivo es: ${file.name}`
+
+}
